9c96d66d6786ce3b7eea818f8dbd648da2ea6139
Flore <floremeyerprevost@gmail.com>
2022-01-02 22:45:35 +0100

    Add animation layers

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 public class PlayerAnimations : MonoBehaviour
 {
     public float stickLerpTime;
+    public float aimLerpTime;
 
+    private Coroutine stickLerpCoroutine;
+    private Coroutine aimLerpCoroutine;
     private Animator animator;
     private Vector2 computedStickDirection, realStickDirection;
 
@@ -14,11 +17,23 @@ public class PlayerAnimations : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext obj)
     {
-        realStickDirection = value.Get<Vector2>();
-        StopCoroutine(StickLerp());
-        StartCoroutine(StickLerp());
+        if(obj.started)
+            return;
+        realStickDirection = obj.ReadValue<Vector2>();
+        if(stickLerpCoroutine != null)
+            StopCoroutine(stickLerpCoroutine);
+        stickLerpCoroutine = StartCoroutine(StickLerp());
+    }
+
+    public void OnAim(InputAction.CallbackContext obj)
+    {
+        if(obj.started)
+            return;
+        if(aimLerpCoroutine != null)
+            StopCoroutine(aimLerpCoroutine);
+        aimLerpCoroutine = StartCoroutine(AimLerp(obj.performed));
     }
 
     private void Update()
@@ -26,6 +41,22 @@ public class PlayerAnimations : MonoBehaviour
         animator.SetFloat("StickX", computedStickDirection.x);
         animator.SetFloat("StickY", computedStickDirection.y);
         animator.SetFloat("Speed", computedStickDirection.sqrMagnitude);
+    }
+
+    private IEnumerator AimLerp(bool isAiming)
+    {
+        var targetValue = isAiming ? 1f : 0f;
+        var startValue = animator.GetLayerWeight(1);
+        var startTime = Time.time;
+        var endTime = startTime + aimLerpTime;
+        while (Time.time < endTime)
+        {
+            var currentLerpTime = Time.time - startTime;
+            var lerpFactor = currentLerpTime / aimLerpTime;
+            var lerp = Mathf.Lerp(startValue, targetValue, lerpFactor);
+            animator.SetLayerWeight(1, lerp);
+            yield return null;
+        }
     }
 
     private IEnumerator StickLerp()
